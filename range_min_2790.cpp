1a88a3fef06bccacc1b0c358b5feefbf1a522147
varvara <va.fedorova2006@gmail.com>
2023-02-04 12:59:49 +0300

    program modified

--- range_min_2790.cpp
+++ range_min_2790.cpp
@@ -2,18 +2,17 @@
 #include <vector>
 #include <cassert>
 #include <algorithm>
+#include <sstream>
 
 using namespace std;
 
 // Добавить элемент x после i-го элемента списка. Если i = 0, то элемент добавляется в начало массива.
 void add_elem(vector<int> &a, int i, int x) { 
-    vector<int> copy_of_a = a;
-    copy_of_a.push_back(0);
-    copy_of_a[i] = x;
-    for (int j = i; j < a.size(); j++) {
-        copy_of_a[j + 1] = a[j];
+    a.push_back(0);
+    for (int j = a.size() - 1; j > i; j--) {
+        a[j] = a[j - 1];
     }
-    a = copy_of_a;
+    a[i] = x;
 }
 
 
@@ -21,26 +20,28 @@ int main() {
     int n;
     cin >> n;
     vector<int> a;
-    vector<int> b;
+    ostringstream os;
     a.reserve(n);
     for (int k = 0; k < n; k++) {
         char c;
         int i, j, x;
         cin >> c >> i >> j;
         if (c == '?') {
-            b.push_back(min(a[i - 1], a[j - 1]));
+            assert(i >= 1 and i <= a.size());
+            assert(j >= 1 and j <= a.size());
+            os << min(a[i - 1], a[j - 1]) << "\n";
         }
         else if (c == '+') {
             x = j;
             add_elem(a, i, x); // добавить элемент x после i-го элемента списка
+           //for (int i = 0; i < a.size(); ++i) {
+           //    cout << a[i] << " ";
+           //}
+           //cout << endl;
         }
         else {
             assert(false);
         }
     }
-    if (b.size() != 0) {
-        for (int i = 0; i < b.size(); i++) {
-            cout << b[i] << endl;
-        }
-    }
+    cout << os.str();
 }
